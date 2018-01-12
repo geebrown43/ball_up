9c8e20c3b79738d2c71d7526d32f7c5c6daadcff
Ellis Mwekunda <Scareb15@gmail.com>
2018-01-11 22:36:24 -0700

    added Map Component to App

--- components/SecondPage.js
+++ components/SecondPage.js
@@ -1,29 +1,55 @@
-import React from 'react'
-import {List, ListItem} from 'react-native-elements'
-import {Text, ScrollView} from 'react-native'
-import MapView from 'react-native-maps'
+import React from "react";
+import { List, ListItem } from "react-native-elements";
+import { Text, ScrollView, View, StyleSheet } from "react-native";
+import { MapView } from "expo";
 
+export default class SecondPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <View>
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            region={{
+              latitude: 39.742043,
+              longitude: -104.991531,
+              latitudeDelta: 0.0912,
+              longitudeDelta: 0.04
+            }}
+          />
+        </View>
 
-export default class SecondPage extends React.Component{
-    constructor(props){
-        super(props)
+        <View style={{ top: 340 }}>
+          <ScrollView>
+            <List containerStyle={{ marginBottom: 20 }}>
+              {this.props.data.map((item, i) => (
+                <ListItem
+                  roundAvatar
+                  avatar={{ uri: item.image }}
+                  key={i}
+                  title={item.name}
+                  subtitle={item.address}
+                />
+              ))}
+            </List>
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+}
 
-    }
-
-    render(){
-        return (
-            <ScrollView>
-                       
-                <List containerStyle={{marginBottom: 20}}>
-                    { this.props.data.map((item, i) => (
-                    <ListItem roundAvatar avatar={{uri:item.icon}}  key={i} title={item.name} subtitle={item.vicinity}/> )) }
-                </List>
-            </ScrollView>
-
-                
-        )
-    }
-}                                
-
-
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
