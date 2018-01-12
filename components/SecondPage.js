0b44319b0d971a4a9b8a71706b7d89be7a0902a2
Ellis Mwekunda <Scareb15@gmail.com>
2018-01-11 22:56:37 -0700

    added code for Marker

--- components/SecondPage.js
+++ components/SecondPage.js
@@ -2,10 +2,14 @@ import React from "react";
 import { List, ListItem } from "react-native-elements";
 import { Text, ScrollView, View, StyleSheet } from "react-native";
 import { MapView } from "expo";
+import Data from './Data'
 
 export default class SecondPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state ={
+        data: Data
+    }
   }
 
   render() {
@@ -17,16 +21,17 @@ export default class SecondPage extends React.Component {
             region={{
               latitude: 39.742043,
               longitude: -104.991531,
-              latitudeDelta: 0.0912,
+              latitudeDelta: 0.1,
               longitudeDelta: 0.04
-            }}
-          />
+            }}>
+            {this.state.data.map(a => console.log(a))}
+            </MapView>
         </View>
 
         <View style={{ top: 340 }}>
           <ScrollView>
             <List containerStyle={{ marginBottom: 20 }}>
-              {this.props.data.map((item, i) => (
+              {this.state.data.map((item, i) => (
                 <ListItem
                   roundAvatar
                   avatar={{ uri: item.image }}
@@ -47,7 +52,8 @@ const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: "flex-end",
-    alignItems: "center"
+    alignItems: "center",
+    bottom:350,
   },
   map: {
     ...StyleSheet.absoluteFillObject
