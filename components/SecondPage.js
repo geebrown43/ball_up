d1a3d1ccd8996bf5e1f230e6e2d7c998b5f27ef6
Ellis Mwekunda <Scareb15@gmail.com>
2018-01-13 14:09:22 -0700

    added mapStyle

--- components/SecondPage.js
+++ components/SecondPage.js
@@ -2,67 +2,48 @@ import React from "react";
 import { List, ListItem } from "react-native-elements";
 import { Text, ScrollView, View, StyleSheet } from "react-native";
 import { MapView } from "expo";
-import Data from './Data'
+import Data from "./Data";
+import CustomMap from'./MapStyle'
 
 export default class SecondPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state ={
-        data: Data
-    }
+    this.state = {
+      data: Data,
+      
+    };
   }
 
   render() {
-    console.log(this.props)
+    console.log(this.props);
     return (
-      <View>
-        <View style={styles.container}>
-          <MapView
-            style={styles.map}
-            region={{
-              latitude: this.props.data.lat,
-              longitude: this.props.data.lng,
-              latitudeDelta: 0.16,
-              longitudeDelta: 0.04
-            }}>
-            {this.state.data.map(a => <MapView.Marker key={a.id} coordinate={{latitude: a.latitude, longitude: a.longitude}}>
+      <View style={{position: 'relative', height: '100%'}}>
+        <MapView
+          style={{top: 0, bottom: 0, right: 0, left: 0, position: 'absolute'}}
+          zoomEnabled ={true}
+          customMapStyle={CustomMap}
+          region={{
+            latitude: this.props.data.lat,
+            longitude: this.props.data.lng,
+            latitudeDelta: 0.16,
+            longitudeDelta: 0.04
+          }}
+        >
+          {this.state.data.map(a => (
+            <MapView.Marker
+              key={a.id}
+              coordinate={{ latitude: a.latitude, longitude: a.longitude }}
+            >
               <MapView.Callout>
                 <Text>{a.name}</Text>
                 <Text>{a.address}</Text>
               </MapView.Callout>
-              
-              </MapView.Marker>)}
-            </MapView>
-        </View>
-
-        <View style={{ top: 340 }}>
-          <ScrollView>
-            <List containerStyle={{ marginBottom: 20 }}>
-              {this.state.data.map((item, i) => (
-                <ListItem
-                  roundAvatar
-                  avatar={{ uri: item.image }}
-                  key={i}
-                  title={item.name}
-                  subtitle={item.address}
-                />
-              ))}
-            </List>
-          </ScrollView>
-        </View>
+            </MapView.Marker>
+          ))}
+        </MapView>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: "flex-end",
-    alignItems: "center",
-    bottom:350,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject
-  }
-});
+
