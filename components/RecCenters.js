61be125cb123f207ddd0a375b4155fdecf2812c9
Gavin Brown <scareb15@gmail.com>
2018-01-13 18:57:11 -0700

    trial page

---
+++ components/RecCenters.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+
+export default class Centers extends React.Component{
+    render(){
+        return (
+            <View>
+                <Text>
+                    Third Page
+                </Text>
+            </View>
+        )
+    }
+}
