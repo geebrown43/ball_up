68981e8556ecec4d6c84594bf834285db138279d
Gavin Brown <scareb15@gmail.com>
2018-01-15 21:24:00 -0700

    added styling to card and it finally works correct

--- components/CourtCard.js
+++ components/CourtCard.js
@@ -1,32 +1,44 @@
 import React from 'react'
-import {ScrollView, Text, Button, View} from 'react-native'
+import { ScrollView, Text, Button, View, Image, StyleSheet } from 'react-native'
 import Modal from 'react-native-modal'
 
 
 
 
-export default class Card extends React.Component{
-    constructor(props){
+export default class Card extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
 
         }
     }
 
-    render(){
-        //console.log(this.props)
+    render() {
+        let data = this.props.data.a
         return (
             <View>
                 <Modal isVisible={this.props.isVisible}>
-                    <View style={{height: 500, justifyContent: 'center'}}>
-                    <ScrollView style={{backgroundColor: 'white', borderRadius: 15}}>
-                    <Text>Name</Text>
-                    <Button title='close' onPress={this.props._showModal}>Close</Button>
-                    </ScrollView>
+                    <View style={{ height: 500, justifyContent: 'center' }}>
+                        <ScrollView style={{ backgroundColor: 'white', borderRadius: 15 }}>
+                            <View style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center'}}>
+                                <Text style={styles.header}>{data.name}</Text>
+                                <Text>{data.address} {data.city}, {data.state}</Text>
+                                <Image source={{ uri: data.image }} style={{ height: 300, width: 300, margin: 10 }} />
+                            </View>
+                            <Button title='close' onPress={this.props._showCard}>Close</Button>
+                        </ScrollView>
                     </View>
-                    
+
                 </Modal>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    header: {
+        fontSize: 28,
+        textAlign: 'center',
+        margin: 5
+    }
+})
